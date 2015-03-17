a69530d593c207483a902c4aac9218dc19dc6462
Adam L. Taylor <taylora@janelia.hhmi.org>
2015-03-17 12:26:34 -0400

    develop: Added test cases to test digital output via WS model.

--- +ws/+test/+hw/BugWith8AIsAndDisplayCausingMissedTriggersTestCase.m
+++ +ws/+test/+hw/BugWith8AIsAndDisplayCausingMissedTriggersTestCase.m
@@ -56,6 +56,9 @@ classdef BugWith8AIsAndDisplayCausingMissedTriggersTestCase < matlab.unittest.Te
                 end
             end                   
 
+            % Delete the data file
+            delete(dataDirNameAbsolute);
+            
             self.verifyEqual(wsModel.ExperimentCompletedTrialCount,nTrials);
             
             %wsController.quit();
