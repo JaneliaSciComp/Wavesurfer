c65680691ca568775ae07a9034be33b984332ca8
Ben Arthur <arthurb@hhmi.org>
2015-04-02 10:28:44 -0400

    feature/raster: added velocity and subthreshold potential vs spatial position to continuous raster

--- +ws/+examples/rasterContinuous.m
+++ +ws/+examples/rasterContinuous.m
@@ -4,8 +4,12 @@ function rasterContinuous(self,evt)
 %   in UserFunctions dialog under Data Available put ws.examples.rasterContinuous
 %   adjust the thresh, nbins, etc. variables below to suite
 
-persistent rasterFig rasterAxes rasterLine histAxes histBars lap initialPosition
-persistent spikeRateAxes spikeRateAverageLine binTimes allBinTimes
+persistent rasterFig lap initialPosition
+persistent rasterAxes rasterLine
+persistent nSpikesAxes nSpikesBars
+persistent velocityAxes velocityAverageLine binVelocities allBinVelocities
+persistent spikeRateAxes spikeRateAverageLine binDwellTimes allBinDwellTimes
+persistent subthresholdAxes subthresholdAverageLine binSubthresholds allBinSubthresholds
 
 thresh = -15;  % mV
 nBins = 20;
@@ -15,44 +19,59 @@ velocityChannel = 2;
 LEDChannel = 3;
 velocityScale = 10;  % cm/s/V;  from steve: 100 mm/sec per volt
 
-step = treadmillLength / nBins;
-binCenters = step/2 : step : treadmillLength;
+binWidth = treadmillLength / nBins;
+binCenters = binWidth/2 : binWidth : treadmillLength;
 sampleRate = self.Acquisition.SampleRate;
 data = self.Acquisition.getLatestData();
 
 if isempty(rasterFig) || ~ishandle(rasterFig)
     rasterFig = figure();
     position = get(rasterFig,'position');
-    set(rasterFig,'position',position.*[1 0 1 1.5]);
+    set(rasterFig,'position',position.*[1 0 1 2]);
 end
 
 if self.NTimesSamplesAcquiredCalledSinceExperimentStart==1
     clf(rasterFig);
 
     rasterLine=[];
-    rasterAxes = subplot(6,1,[1 2 3],'parent',rasterFig);
+    rasterAxes = subplot(10,1,[1 2 3],'parent',rasterFig);
     hold(rasterAxes, 'on');
     axis(rasterAxes, 'ij');
-    linkaxes([histAxes rasterAxes],'x');
     ylabel(rasterAxes, 'lap #');
     title(rasterAxes, ['channel ' self.Acquisition.ChannelNames{electrodeChannel}]);
     
-    histAxes = subplot(6,1,4,'parent',rasterFig);
-    hold(histAxes, 'on');
-    histBars = bar(histAxes,binCenters,zeros(1,nBins),1);
-    set(histBars,'EdgeColor','none','facecolor',[1 0 0]);
-    ylabel(histAxes, '# spikes');
+    nSpikesAxes = subplot(10,1,4,'parent',rasterFig);
+    hold(nSpikesAxes, 'on');
+    nSpikesBars = bar(nSpikesAxes,binCenters,zeros(1,nBins),1);
+    set(nSpikesBars,'EdgeColor','none','facecolor',[1 0 0]);
+    ylabel(nSpikesAxes, '# spikes');
 
-    spikeRateAxes = subplot(6,1,[5 6],'parent',rasterFig);
+    velocityAxes = subplot(10,1,[5 6],'parent',rasterFig);
+    hold(velocityAxes, 'on');
+    velocityAverageLine = plot(velocityAxes,binCenters,nan(1,length(binCenters)),'ro-');
+    ylabel(velocityAxes, 'velocity (cm/s)');
+
+    spikeRateAxes = subplot(10,1,[7 8],'parent',rasterFig);
     hold(spikeRateAxes, 'on');
     spikeRateAverageLine = plot(spikeRateAxes,binCenters,nan(1,length(binCenters)),'ro-');
     ylabel(spikeRateAxes, 'spike rate (/s)');
-    xlabel(spikeRateAxes, 'position on treadmill (cm)');
 
+    subthresholdAxes = subplot(10,1,[9 10],'parent',rasterFig);
+    hold(subthresholdAxes, 'on');
+    subthresholdAverageLine = plot(subthresholdAxes,binCenters,nan(1,length(binCenters)),'ro-');
+    ylabel(subthresholdAxes, 'subthreshold (mV)');
+    xlabel(subthresholdAxes, 'position on treadmill (cm)');
+
+    linkaxes([rasterAxes nSpikesAxes spikeRateAxes],'x');
+    
     lap=1;
     initialPosition=0;
-    binTimes=zeros(1,length(nBins));
-    allBinTimes=zeros(1,length(nBins));
+    binDwellTimes=zeros(1,nBins);
+    allBinDwellTimes=zeros(1,nBins);
+    binVelocities=cell(1,nBins);
+    allBinVelocities=cell(1,nBins);
+    binSubthresholds=cell(1,nBins);
+    allBinSubthresholds=cell(1,nBins);
 end
 
 boundary = find(data(:,LEDChannel)<0.5,1);
@@ -64,8 +83,16 @@ ticks = find(diff(data(:,electrodeChannel)>thresh)==1);
 integratedVelocity = cumsum(data(:,velocityChannel)*velocityScale/sampleRate);
 rasterLine = [rasterLine; ...
         initialPosition+integratedVelocity(find(ticks<boundary))];
-binTimes = binTimes + hist(initialPosition+integratedVelocity, binCenters)./sampleRate;
-    
+binDwellTimes = binDwellTimes + hist(initialPosition+integratedVelocity, binCenters)./sampleRate;
+for i=1:length(binCenters)
+  binVelocities{i} = [binVelocities{i}; ...
+        data(abs(initialPosition+integratedVelocity-binCenters(i))<binWidth,velocityChannel)];
+end
+for i=1:length(binCenters)
+  binSubthresholds{i} = [binSubthresholds{i}; ...
+        data(abs(initialPosition+integratedVelocity-binCenters(i))<binWidth,electrodeChannel)];
+end
+
 if  boundary < size(data,1)+1
     len = length(rasterLine);
     plot(rasterAxes, ...
@@ -74,23 +101,52 @@ if  boundary < size(data,1)+1
             'k-');
     axis(rasterAxes, [0 treadmillLength 0 lap+eps]);
     
-    histData = hist(rasterLine, binCenters);
-    allHistData = get(histBars, 'YData') + histData;
-    set(histBars, 'YData', allHistData);
-    axis(histAxes, [0 treadmillLength 0 max([allHistData 0])+eps]);
+    nSpikesData = hist(rasterLine, binCenters);
+    allNSpikesData = get(nSpikesBars, 'YData') + nSpikesData;
+    set(nSpikesBars, 'YData', allNSpikesData);
+    axis(nSpikesAxes, [0 treadmillLength 0 max([allNSpikesData 0])+eps]);
     
-    set(spikeRateAverageLine, 'ydata', allHistData./allBinTimes);
+    for i=1:length(binCenters)
+      allBinVelocities{i} = [allBinVelocities{i}; binVelocities{i}];
+    end
+    allMeanVelocities = cellfun(@(x) mean(x),allBinVelocities);
+    set(velocityAverageLine, 'ydata', allMeanVelocities);
+    velocityLines=get(velocityAxes,'children');
+    arrayfun(@(x) set(x,'color',[0.75 0.75 0.75]), setdiff(velocityLines,velocityAverageLine));
+    meanVelocities = cellfun(@(x) mean(x),binVelocities);
+    plot(velocityAxes,binCenters,meanVelocities,'k.-');
+    uistack(velocityAverageLine,'top');
+    axis(velocityAxes);
+    axis(velocityAxes, [0 treadmillLength 0 max([ans(4) allMeanVelocities meanVelocities])+eps]);
+    
+    allBinDwellTimes = allBinDwellTimes + binDwellTimes;
+    set(spikeRateAverageLine, 'ydata', allNSpikesData./allBinDwellTimes);
     spikeRateLines=get(spikeRateAxes,'children');
     arrayfun(@(x) set(x,'color',[0.75 0.75 0.75]), setdiff(spikeRateLines,spikeRateAverageLine));
-    plot(spikeRateAxes,binCenters,histData./binTimes,'k.-');
+    plot(spikeRateAxes,binCenters,nSpikesData./binDwellTimes,'k.-');
     uistack(spikeRateAverageLine,'top');
     axis(spikeRateAxes);
-    axis(spikeRateAxes, [0 treadmillLength 0 max([ans(4) allHistData./allBinTimes histData./binTimes])+eps]);
+    axis(spikeRateAxes, [0 treadmillLength 0 max([ans(4) allNSpikesData./allBinDwellTimes nSpikesData./binDwellTimes])+eps]);
+    
+    for i=1:length(binCenters)
+      allBinSubthresholds{i} = [allBinSubthresholds{i}; binSubthresholds{i}];
+    end
+    allMeanSubthresholds = cellfun(@(x) median(x),allBinSubthresholds);
+    set(subthresholdAverageLine, 'ydata', allMeanSubthresholds);
+    subthresholdLines=get(subthresholdAxes,'children');
+    arrayfun(@(x) set(x,'color',[0.75 0.75 0.75]), setdiff(subthresholdLines,subthresholdAverageLine));
+    meanSubthresholds = cellfun(@(x) median(x),binSubthresholds);
+    plot(subthresholdAxes,binCenters,meanSubthresholds,'k.-');
+    uistack(subthresholdAverageLine,'top');
+    axis(subthresholdAxes);
+    axis(subthresholdAxes, [0 treadmillLength ...
+        min([ans(4) allMeanSubthresholds meanSubthresholds]) max([ans(4) allMeanSubthresholds meanSubthresholds])+eps]);
     
     lap = lap + 1;
     initialPosition = -integratedVelocity(boundary);
-    allBinTimes = allBinTimes + binTimes;
-    binTimes=zeros(1,length(nBins));
+    binDwellTimes=zeros(1,nBins);
+    binVelocities=cell(1,nBins);
+    binSubthresholds=cell(1,nBins);
     rasterLine = initialPosition + integratedVelocity(find(ticks>=boundary));
 end
 
