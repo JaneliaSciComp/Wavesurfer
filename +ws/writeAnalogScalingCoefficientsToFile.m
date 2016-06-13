556c4627496f76653dbdbb59fde7c8b34476ee5a
Ackerman <ackermand@hhmi.org>
2016-06-13 17:52:14 -0400

    feature/ReadWriteCalibrationData: Updated name of input from outputFileName to outputFilePath.

--- +ws/writeAnalogScalingCoefficientsToFile.m
+++ +ws/writeAnalogScalingCoefficientsToFile.m
@@ -1,11 +1,11 @@
-function writeAnalogScalingCoefficientsToFile(deviceName, outputFileName)
+function writeAnalogScalingCoefficientsToFile(deviceName, outputFilePath)
     % Writes scaling coefficients from device deviceName to file outputFileName.
-    outputFileName = fullfile(outputFileName);
-    [pathString, fileName, extension] = fileparts(outputFileName);
-    if exist(outputFileName,'file')
+    outputFilePath = fullfile(outputFilePath);
+    [pathString, fileName, extension] = fileparts(outputFilePath);
+    if exist(outputFilePath,'file')
         error('ws:fileNameExists',...
               '''%s'' already exists',...
-              outputFileName);
+              outputFilePath);
     else % outputFileName is available
         fullFilePathAsMatFile=fullfile(pathString,[fileName,'.mat']); % Ensure it will be .mat file
         if ~strcmp(extension,'.mat')
@@ -18,7 +18,7 @@ function writeAnalogScalingCoefficientsToFile(deviceName, outputFileName)
             else
                 warning('ws:fileExtensionChanged',...
                         'Will attempt to save as ''%s'', not ''%s''',...
-                        fullFilePathAsMatFile, outputFileName);
+                        fullFilePathAsMatFile, outputFilePath);
             end
         end
         % Read and save the scaling coefficients to a .mat file
