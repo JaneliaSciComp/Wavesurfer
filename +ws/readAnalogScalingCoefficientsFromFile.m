262ece46cc76c317e614116676862a4f3d22e6b4
Ackerman <ackermand@hhmi.org>
2016-06-15 09:58:53 -0400

    develop: Updated readAnalogScalingCoefficientsFromFile to allow it to read in files with any extension

--- +ws/readAnalogScalingCoefficientsFromFile.m
+++ +ws/readAnalogScalingCoefficientsFromFile.m
@@ -1,36 +1,33 @@
-function scalingCoefficients = readAnalogScalingCoefficientsFromFile(scalingCoefficientsFilePath)
+function outputScalingCoefficients = readAnalogScalingCoefficientsFromFile(sanitizedScalingCoefficientsFilePath)
     % Reads the scaling coefficients from a file with name
     % scalingCoefficientsFilePath, and returns them in scalingCoefficients.
-    scalingCoefficientsFilePath = fullfile( scalingCoefficientsFilePath );
-    [~, ~, extension] = fileparts(scalingCoefficientsFilePath);
-    if exist(scalingCoefficientsFilePath,'file')~=2 % Then it is not a file
-        error('ws:noSuchFileOrDirectory',...
+    
+    sanitizedScalingCoefficientsFilePath = fullfile( sanitizedScalingCoefficientsFilePath );
+    if exist(sanitizedScalingCoefficientsFilePath,'file')~=2 % Then it is not a file
+        error('ws:noSuchFile',...
               'Unable to read ''%s'': no such file',...
-              scalingCoefficientsFilePath);
-    elseif ~strcmp(extension,'.mat')
-        error('ws:incorrectFileType',...
-              'Incorrect file type: needs to have ''.mat'' extension, not ''%s''', extension);
+              sanitizedScalingCoefficientsFilePath);
     else
         % The file exists, load it into fileData
-        fprintf('Reading data from file ''%s''\n', scalingCoefficientsFilePath);
-        fileData = load(scalingCoefficientsFilePath);
+        fprintf('Reading data from file ''%s''\n', sanitizedScalingCoefficientsFilePath);
+        fileData = load(sanitizedScalingCoefficientsFilePath, '-mat');
         % If the file was generated from
         % ws.writeAnalogScalingCoefficientsToFile, then the only field
         % should be scalingCoefficients
         if isfield(fileData,'scalingCoefficients') == 0
             error('ws:noScalingCoefficientsInFile',...
                   'File ''%s'' does not contain scalingCoefficients variable',...
-                  scalingCoefficientsFilePath);
+                  sanitizedScalingCoefficientsFilePath);
         elseif isempty(fileData.scalingCoefficients)
             error('ws:scalingCoefficientsEmpty',...
                   'File ''%s'' contains an empty scalingCoefficients variable',...
-                  scalingCoefficientsFilePath);
+                  sanitizedScalingCoefficientsFilePath);
         elseif length( fieldnames(fileData) ) > 1
             error('ws:tooManyVariablesInFile',...
                   'File ''%s'' has has too many workspace variables',...
-                  scalingCoefficientsFilePath);
+                  sanitizedScalingCoefficientsFilePath);
         else
-            scalingCoefficients = fileData.scalingCoefficients;
+            outputScalingCoefficients = fileData.scalingCoefficients;
         end
     end
 end
