4871f3153324008869ea98528ef504e54a8e740c
Adam L. Taylor <taylora@janelia.hhmi.org>
2016-06-27 11:07:33 -0400

    release: Added debug() method to ws.WavesurferModel, clear more ZMQ objects in delete() method, though shouldn't matter.

--- +ws/WavesurferModel.m
+++ +ws/WavesurferModel.m
@@ -339,7 +339,10 @@ classdef WavesurferModel < ws.RootModel
 
                 % Close the sockets
                 self.LooperIPCSubscriber_ = [] ;
-                self.IPCPublisher_ = [] ;                
+                self.RefillerIPCSubscriber_ = [] ;
+                self.LooperIPCRequester_ = [] ;
+                self.RefillerIPCRequester_ = [] ;
+                self.IPCPublisher_ = [] ;
             end
             
             %if ~isempty(self) ,
@@ -2676,6 +2679,10 @@ classdef WavesurferModel < ws.RootModel
             %fprintf('In WavesurferModel:determineKeystoneTasks():\n') ;
             %fprintf('  acquisitionKeystoneTask: %s\n', acquisitionKeystoneTask) ;
             %fprintf('  stimulationKeystoneTask: %s\n\n', stimulationKeystoneTask) ;            
+        end
+        
+        function debug(self) %#ok<MANU>
+            keyboard
         end
     end  % public methods block
     
