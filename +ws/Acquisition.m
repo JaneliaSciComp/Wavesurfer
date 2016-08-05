aa97d10cb1badbaf290ed8d24758037abba65395
Adam L. Taylor <taylora@janelia.hhmi.org>
2016-08-05 10:56:26 -0400

    bugfix/shared: Fixed main bug responsible for shared structure, also added some methods to Acquistion to allow getting/setting of protected properties.

--- +ws/Acquisition.m
+++ +ws/Acquisition.m
@@ -273,6 +273,17 @@ classdef Acquisition < ws.AcquisitionSubsystem
     end
 
     methods (Access=protected)
+        function out = getPropertyValue_(self, name)
+            out = self.(name);
+        end  % function
+        
+        % Allows access to protected and protected variables from ws.Coding.
+        function setPropertyValue_(self, name, value)
+            self.(name) = value;
+        end  % function
+    end
+    
+    methods (Access=protected)
         function result = getAnalogScalingCoefficients_(self)
             result = self.AnalogScalingCoefficientsCache_ ;
         end
