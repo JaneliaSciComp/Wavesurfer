02ac5e981542aff6bcf252e0c059b097e0f9f408
Adam L. Taylor <taylora@hhmi.org>
2016-08-04 22:57:39 -0400

    develop: Added test of WSM independence after calling mimic(), currently fails.

--- +ws/Coding.m
+++ +ws/Coding.m
@@ -1,6 +1,14 @@
 classdef (Abstract) Coding < handle
 
     methods
+        function propNames = listPropertiesForCheckingIndependence(self)
+            % Define a helper function
+            doesNeedToBeChecked = @(x)(~x.Dependent && ~x.Constant) ;
+
+            % Actually get the prop names that satisfy the predicate
+            propNames = self.propertyNamesSatisfyingPredicate_(doesNeedToBeChecked);
+        end
+        
         function propNames = listPropertiesForPersistence(self)
             % Define a helper function
             shouldPropertyBePersisted = @(x)(~x.Dependent && ~x.Transient && ~x.Constant) ;
@@ -63,6 +71,50 @@ classdef (Abstract) Coding < handle
             end
         end
 
+        function result = isIndependentFrom(self, other)            
+            % Get the list of property names for this file type
+            propertyNames = self.listPropertiesForCheckingIndependence();
+            
+            % Set each property to the corresponding one
+            for i = 1:length(propertyNames) ,
+                thisPropertyName=propertyNames{i};
+                if isequal(thisPropertyName, 'Parent_') ,
+                    % skip to avoid infinite recursion
+                else
+                    selfProperty = self.getPropertyValue_(thisPropertyName) ;
+                    otherProperty = other.getPropertyValue_(thisPropertyName) ;
+                    if isa(selfProperty,'handle') ,
+                        if isa(otherProperty,'handle') ,
+                            if any(selfProperty==otherProperty) ,
+                                fprintf('Failure for property %s\n',thisPropertyName) ;
+                                result = false ;
+                                return
+                            else                                
+                                isThisPropertyIndependent = selfProperty.isIndependentFrom(otherProperty) ;
+                                if isThisPropertyIndependent ,
+                                    % these are independent, so keep checking...
+                                else
+                                    % these are not independent, so we can stop
+                                    % looking
+                                    result = false ;
+                                    return
+                                end
+                            end
+                        else
+                            % source is a value, so must be independent, so nothing
+                            % to do
+                        end                    
+                    else
+                        % target is a value, so must be independent, so nothing
+                        % to do
+                    end
+                end
+            end
+
+            % If we get here, no dependencies were found
+            result = true ;
+        end  % function
+        
         function mimic(self, other)
             % Cause self to resemble other.  This implementation sets all
             % the properties that would get stored to the .cfg file to the
