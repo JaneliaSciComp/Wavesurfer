a1a1fcf0b56b92739cdbf87d4140a047d4ca5bd9
Adam L. Taylor <taylora@janelia.hhmi.org>
2017-01-09 18:59:43 -0500

    develop: Streamlined StickShiftBiasUserClass.m.

--- +ws/+examples/+bias/StickShiftBiasUserClass.m
+++ +ws/+examples/+bias/StickShiftBiasUserClass.m
@@ -22,9 +22,20 @@ classdef StickShiftBiasUserClass < ws.UserClass
             self.isIInFrontend_ = ( isa(userCodeManager.Parent,'ws.WavesurferModel') && userCodeManager.Parent.IsITheOneTrueWavesurferModel ) ;
         end
         
-        function delete(self)
+        function delete(self)            
+            fprintf('Deleting the BIAS user object') ;
             if self.areCameraInterfacesInitialized_ ,
-                self.finalizeBiasCameraInterfaces_() ;
+                for i=1:self.cameraCount_ ,
+                    try
+                        %self.biasCameraInterfaces_{i}.disconnect();
+                    catch me  %#ok<NASGU>
+                        % ignore
+                    end
+                    delete(self.biasCameraInterfaces_{i});
+                    self.biasCameraInterfaces_{i} = [] ;  % set the cell to empty, but don't change length of cell array
+                end
+                self.biasCameraInterfaces_ = cell(1,0) ;  % set to zero-length cell array
+                self.areCameraInterfacesInitialized_ = false ;
             end
         end
         
@@ -32,7 +43,28 @@ classdef StickShiftBiasUserClass < ws.UserClass
             fprintf('Starting a run.\n');
             if self.isIInFrontend_ ,
                 if ~self.areCameraInterfacesInitialized_ ,
-                    self.initializeBiasCameraInterfaces_() ;
+                    cameraCount = 0 ; %#ok<PROPLC>
+                    for iCamera = 1:16 ,  % no one will have more cameras than this
+                        portNumber = self.serverFirstCameraPort + (iCamera-1)*self.serverPortStride ;
+                        cameraInterface = ws.examples.bias.SimpleBiasCameraInterface(self.serverIPAddressAsString, portNumber) ;
+                        try
+                            %cameraInterface.connectAndGetConfiguration() ;
+                            response = cameraInterface.getStatus() ; %#ok<NASGU>
+                        catch me
+                            if isequal(me.identifier, 'SimpleBiasCameraInterface:unableToConnectToBIASServer') ,
+                                break ;
+                            else
+                                me.rethrow() ;
+                            end
+                        end
+                        % if get here, must have successfully connected to camera i
+                        cameraCount = iCamera ;  %#ok<PROPLC>
+                        self.biasCameraInterfaces_{1,iCamera} = cameraInterface ;
+                    end
+                    self.cameraCount_ = cameraCount ;  %#ok<PROPLC>
+                    fprintf('Number of cameras found: %d\n', cameraCount) ;  %#ok<PROPLC>
+                    % Finally, set the semaphore
+                    self.areCameraInterfacesInitialized_ = true ;
                 end
             end
         end
@@ -41,33 +73,54 @@ classdef StickShiftBiasUserClass < ws.UserClass
             % Called just before each trial
             fprintf('Starting a sweep.\n');
             if self.isIInFrontend_ ,
-                self.configureForSweep_();
-                self.start_();
+                % Get status from each camera, to make sure BIAS is really
+                % ready to go
+                for i=1:self.cameraCount_ ,
+                    response = self.biasCameraInterfaces_{i}.getStatus() ; 
+                    if ~response.value.connected ,
+                        error('BIAS is not connected to camera %d', i-1) ;
+                    end
+                end
+                for i=1:self.cameraCount_ ,
+                    self.biasCameraInterfaces_{i}.startCapture() ;
+                end
+
+                % Wait for bias to be ready
+                checkInterval = 0.1 ;  % s
+                maxNumberOfChecks = 50 ;
+                for i=1:maxNumberOfChecks ,
+                    areAllCamerasCapturing = true ;
+                    for j=1:self.cameraCount_ ,
+                        response = self.biasCameraInterfaces_{j}.getStatus() ;   % call this just to make sure BIAS is capturing
+                        if ~response.value.capturing ,
+                            areAllCamerasCapturing = false ;
+                            break ;
+                        end
+                    end
+                    if areAllCamerasCapturing ,
+                        break ;
+                    else
+                        pause(checkInterval) ;    % have to wait a bit for both cams to be capturing
+                    end
+                end                
+                % If get here, either all cameras are capturing, or we gave
+                % up waiting.  Should we throw an error if not all cameras are capturing?                
             end
         end
         
         function completingSweep(self, ~, ~)
-            %dbstack;
             fprintf('Completing a sweep.\n');
-            if self.isIInFrontend_ ,
-                self.stop_();
-            end
+            self.completingOrAbortingOrStoppingASweep_();
         end
         
         function abortingSweep(self,~,~)
-            %dbstack;
             fprintf('Oh noes!  A sweep aborted.\n');
-            if self.isIInFrontend_ ,
-                self.stop_();
-            end
+            self.completingOrAbortingOrStoppingASweep_();
         end
         
         function stoppingSweep(self,~,~)
-            %dbstack;
             fprintf('A sweep was stopped.\n');
-            if self.isIInFrontend_ ,
-                self.stop_();
-            end
+            self.completingOrAbortingOrStoppingASweep_();
         end
         
         function completingRun(self,~,~) %#ok<INUSD>
@@ -87,24 +140,18 @@ classdef StickShiftBiasUserClass < ws.UserClass
         end
         
         function dataAvailable(~,~,~)
-            %dbstack;
         end
 
         function startingEpisode(~,~,~)
-            % Called just before each trial
-            fprintf('About to start a sweep.\n');
         end
         
         function completingEpisode(~,~,~)
-            %dbstack;
         end
         
         function abortingEpisode(~,~,~)
-            %dbstack;
         end
         
         function stoppingEpisode(~,~,~)
-            %dbstack;
         end
         
         % These methods are called in the looper process
@@ -118,117 +165,40 @@ classdef StickShiftBiasUserClass < ws.UserClass
         function result = get.cameraCount(self)
             result = self.cameraCount_ ;
         end
-    end  % methods
+    end  % public methods
 
 
     methods (Access=private)
-        function initializeBiasCameraInterfaces_(self)
-            disp('Calling BIAS init.');
-            cameraCount = 0 ;  %#ok<PROP>
-            for iCamera = 1:16 ,  % no one will have more cameras than this
-                portNumber = self.serverFirstCameraPort + (iCamera-1)*self.serverPortStride ;
-                cameraInterface = ws.examples.bias.SimpleBiasCameraInterface(self.serverIPAddressAsString, portNumber) ;
-                try
-                    %cameraInterface.connectAndGetConfiguration() ;
-                    response = cameraInterface.getStatus() ; %#ok<NASGU>
-                catch me
-                    if isequal(me.identifier, 'SimpleBiasCameraInterface:unableToConnectToBIASServer') ,
-                        break ;
-                    else
-                        me.rethrow() ;
-                    end
-                end
-                % if get here, must have successfully connected to camera i
-                cameraCount = iCamera ;  %#ok<PROP>
-                self.biasCameraInterfaces_{1,iCamera} = cameraInterface ;
-            end
-            self.cameraCount_ = cameraCount ;  %#ok<PROP>
-            fprintf('Number of cameras found: %d\n', cameraCount) ;  %#ok<PROP>
-%             % Get status from each camera, to make sure BIAS is really
-%             % ready to go
-%             for iCamera = 1:cameraCount ,
-%                 self.biasCameraInterfaces_{iCamera}.getStatus() ;
-%             end
-            % Finally, set the semaphore
-            self.areCameraInterfacesInitialized_ = true ;
-        end
-
-        function configureForSweep_(self)
-            disp('Calling BIAS config.');
-            % Get status from each camera, to make sure BIAS is really
-            % ready to go
-            for i=1:self.cameraCount_ ,
-                response = self.biasCameraInterfaces_{i}.getStatus() ; 
-                if ~response.value.connected ,
-                    error('BIAS is not connected to camera %d', i-1) ;
-                end
-            end
-        end
-        
-        function start_(self)
-            disp('Calling BIAS start.');
-            for i=1:self.cameraCount_ ,
-                self.biasCameraInterfaces_{i}.startCapture() ;
-            end
-
-            % wait for bias to be ready
-            checkInterval = 0.1 ;  % s
-            maxNumberOfChecks = 1000 ;
-            for i=1:maxNumberOfChecks ,
-                areAllCamerasCapturing = true ;
-                for j=1:self.cameraCount_ ,
-                    response = self.biasCameraInterfaces_{j}.getStatus() ;   % call this just to make sure BIAS is capturing
-                    if ~response.value.capturing ,
-                        areAllCamerasCapturing = false ;
-                        break ;
-                    end
-                end
-                if areAllCamerasCapturing ,
-                    break ;
-                else
-                    pause(checkInterval) ;    % have to wait a bit for both cams to be capturing
-                end
-            end
-        end  % method        
-                
-        function stop_(self)
+        function completingOrAbortingOrStoppingASweep_(self)
             % wait for bias to be done
-            checkInterval = 0.1 ;  % s
-            maxNumberOfChecks = 10 ;
-            for i=1:maxNumberOfChecks ,
-                isACameraCapturing = false ;
-                for j=1:self.cameraCount_
-                    response = self.biasCameraInterfaces_{j}.getStatus() ;   % call this just to make sure BIAS is done
-                    if response.value.capturing ,
-                        isACameraCapturing = true ;
+            if self.isIInFrontend_ ,
+                % Tell bias to stop capturing
+                for i=1:self.cameraCount_ ,
+                    self.biasCameraInterfaces_{i}.stopCapture() ;
+                end
+
+                % Wait for it to stop capturing
+                checkInterval = 0.1 ;  % s
+                maxNumberOfChecks = 50 ;
+                for i=1:maxNumberOfChecks ,
+                    isACameraCapturing = false ;
+                    for j=1:self.cameraCount_
+                        response = self.biasCameraInterfaces_{j}.getStatus() ;   % call this just to make sure BIAS is done
+                        if response.value.capturing ,
+                            isACameraCapturing = true ;
+                            break ;
+                        end
+                    end
+                    if isACameraCapturing ,
+                        pause(checkInterval) ;    % have to wait a bit for all cams to be done
+                    else
                         break ;
                     end
-                end
-                if isACameraCapturing ,
-                    pause(checkInterval) ;    % have to wait a bit for all cams to be done
-                else
-                    break ;
-                end
+                end                
+                % If we get here, either all cameras are stopped or we gave
+                % up waiting.  Should we do something special if we gave up
+                % waiting?
             end
-            disp('Calling BIAS stop.');
-            for i=1:self.cameraCount_ ,
-                self.biasCameraInterfaces_{i}.stopCapture() ;
-            end
-        end
-                
-        function finalizeBiasCameraInterfaces_(self)
-            disp('Calling BIAS finalize.');
-            for i=1:self.cameraCount_ ,
-                try
-                    %self.biasCameraInterfaces_{i}.disconnect();
-                catch me  %#ok<NASGU>
-                    % ignore
-                end
-                delete(self.biasCameraInterfaces_{i});
-                self.biasCameraInterfaces_{i} = [] ;  % set the cell to empty, but don't change length of cell array
-            end
-            self.biasCameraInterfaces_ = cell(1,0) ;  % set to zero-length cell array
-            self.areCameraInterfacesInitialized_ = false ;
         end
     end  % private methods block
     
