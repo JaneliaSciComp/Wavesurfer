6cce06a94fb6341e3e362bb4421097b4dbc886d8
Adam L. Taylor <taylora@janelia.hhmi.org>
2017-04-06 12:04:24 -0400

    hotfix/low-fs: Re-fixed test failure.

--- +ws/+test/+nohw/StimulusMapTestCase.m
+++ +ws/+test/+nohw/StimulusMapTestCase.m
@@ -31,7 +31,7 @@ classdef StimulusMapTestCase < matlab.unittest.TestCase
         function testGetData(self)
             isChannelAnalog = [true true] ;
             data = self.DefaultLibrary.calculateSignalsForMap(1, 100, {'ch1' 'ch2'}, isChannelAnalog) ;
-            self.verifyTrue(all(size(data) == [101 2]), 'Incorrect data size.');
+            self.verifyTrue(all(size(data) == [100 2]), 'Incorrect data size.');
         end
     end
     
