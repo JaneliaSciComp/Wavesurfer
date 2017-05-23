9613ca58eb8b4950bebe6d6e84819809131bf01e
Adam L. Taylor <taylora@janelia.hhmi.org>
2017-05-23 13:52:18 -0400

    develop: Fixed some test failures with testing whether a device is a PXI device.

--- +ws/isDeviceAPXIDevice.m
+++ +ws/isDeviceAPXIDevice.m
@@ -1,5 +1,18 @@
 function result = isDeviceAPXIDevice(deviceName) 
-    device = ws.dabs.ni.daqmx.Device(deviceName) ;
-    busType = get(device, 'busType') ;
-    result = ismember(busType, {'DAQmx_Val_PXI' 'DAQmx_Val_PXIe'}) ;
+    if isempty(deviceName) ,
+        result = false ;
+    else
+        try
+            device = ws.dabs.ni.daqmx.Device(deviceName) ;
+        catch exception
+            if isequal(exception.identifier,'dabs:noDeviceByThatName') ,
+                result = false ;
+                return
+            else
+                rethrow(exception) ;
+            end
+        end
+        busType = get(device, 'busType') ;
+        result = ismember(busType, {'DAQmx_Val_PXI' 'DAQmx_Val_PXIe'}) ;
+    end
 end
