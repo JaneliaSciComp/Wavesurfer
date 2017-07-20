c0c3dea33a5e0cba4f78814bbb0018f93e8223f9
Adam L. Taylor <taylora@janelia.hhmi.org>
2017-07-20 18:19:21 -0400

    develop: Can now create a single DI chan with multiple arbitrary lines in it.

--- +ws/+dabs/+ni/+daqmx/+private/createDIOChanIDArrays.m
+++ +ws/+dabs/+ni/+daqmx/+private/createDIOChanIDArrays.m
@@ -3,12 +3,21 @@ function [physChanNameArray,chanNameArray] = createDIOChanIDArrays(numChans, dev
         physChanIDs = {physChanIDs};
     end
 
+    % define local function to capture deviceName
+    function result = appendDeviceName(str) 
+        result = horzcat(deviceName, '/', str) ;
+    end
+    
     physChanNameArray = cell(1, numChans) ;
     chanNameArray = cell(1, numChans) ;
     for iChan = 1:numChans ,
-        thisPhysChanID = physChanIDs{iChan} ;
-        portOrLineID = regexpi(thisPhysChanID, '\s*/?(.*)','tokens','once') ;  % Removes leading slash, if any  
-        thisPhysChanName = [deviceName '/' portOrLineID{1}] ;
+        thisPhysChanID = physChanIDs{iChan} ;  % e.g. 'port0/line0', or 'port0/line0, port0/line1', or 'line0', or 'line0, line4'
+        thisPhysChanIDAsCellString = strtrim(strsplit(thisPhysChanID, ',')) ;
+        portOrLineIDsAsCellString = cellfun(@removeLeadingSlash, thisPhysChanIDAsCellString, 'UniformOutput', false) ;
+        thisPhysChanNameAsCellString = cellfun(@appendDeviceName, portOrLineIDsAsCellString, 'UniformOutput', false) ;
+        thisPhysChanName = stringFromCellString(thisPhysChanNameAsCellString) ;        
+        %portOrLineID = regexpi(thisPhysChanID, '\s*/?(.*)','tokens','once') ;  % Removes leading slash, if any  
+        %thisPhysChanName = [deviceName '/' portOrLineID{1}] ;
         if isempty(chanNames)
             thisChanName = ''; 
               % Would prefer to give it the physical chan name, but DAQmx won't take any
@@ -29,5 +38,33 @@ function [physChanNameArray,chanNameArray] = createDIOChanIDArrays(numChans, dev
         % Put the outputs for this channel in the output arrays
         physChanNameArray{iChan} = thisPhysChanName ;
         chanNameArray{iChan} = thisChanName ;
+    end
+end
+
+function result = removeLeadingSlash(str)
+    % Removes a leading slash from string str, if present.  Otherwise returns str.
+    if isempty(str) ,
+        result = str ;
+    else
+        if isequal(str(1), '/') ,
+            result = str(2:end) ;
+        else
+            result = str ;
+        end
+    end
+end
+
+
+function result = stringFromCellString(cellstring)
+    % Converts a cell string to a string, separating values with commas.
+    % E.g. {'foo', 'bar', 'baz'} -> 'foo, bar, baz'
+    if isempty(cellstring) ,
+        result = '' ;
+    else
+        nElements = length(cellstring) ;
+        result = cellstring{1} ;
+        for i = 2:nElements ,
+            result = horzcat(result, ', ', cellstring{i}) ; %#ok<AGROW>
+        end   
     end
 end
