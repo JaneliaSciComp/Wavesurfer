b760db031f254a2f132b862c52105167865db967
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-10-02 18:49:03 -0400

    hotfix/heka-r2018a: Minor change to a test.

--- +ws/+test/+hw/EPCMasterSocketTestCase.m
+++ +ws/+test/+hw/EPCMasterSocketTestCase.m
@@ -17,9 +17,9 @@ classdef EPCMasterSocketTestCase < matlab.unittest.TestCase
         function testReopening(self)
             ems=ws.EPCMasterSocket();
             self.verifyFalse(ems.IsOpen);
-            err1 = ems.open() ;
+            ems.open() ;
             self.verifyTrue(ems.IsOpen);            
-            err2 = ems.reopen() ;
+            ems.reopen() ;
             self.verifyTrue(ems.IsOpen);
             ems.close();
             self.verifyFalse(ems.IsOpen);
