b447f75155b692ff2c31050712321d87a6cefce4
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-01-03 13:07:12 -0500

    feature/skunk2: Cleared out ZMQ stuff, which we don't need anymore.

--- matlab-zmq/lib/+zmq/@Context/set.m
+++
@@ -1,4 +0,0 @@
-function set(obj, name, value)
-    optName = obj.normalize_const_name(name);
-    zmq.core.ctx_set(obj.contextPointer, optName, value);
-end
--- matlab-zmq/lib/+zmq/@Socket/send_string.m
+++
@@ -1,3 +0,0 @@
-function send_string(obj, message, varargin)
-    obj.send_multipart(uint8(message), varargin{:});
-end
