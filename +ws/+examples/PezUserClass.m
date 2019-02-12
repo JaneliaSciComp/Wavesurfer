3aeccddee9119efdea9008236a4d23b8182547f8
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-02-12 18:35:41 -0500

    feature/pez: Added alternation of trial types.

--- +ws/+examples/PezUserClass.m
+++ +ws/+examples/PezUserClass.m
@@ -1,13 +1,24 @@
 classdef PezUserClass < ws.UserClass
     properties
-        BasePosition  % 3x1, mm
-        ToneFrequency  % Hz
+        TrialSequenceMode = 'all 1'  % can also be 'all-1', 'all-2', 'alternating'
+        
+        BasePosition1  % 3x1, mm
+        ToneFrequency1  % Hz
+        DeliverPosition1  % 3x1, mm
+
+        BasePosition2  % 3x1, mm
+        ToneFrequency2  % Hz
+        DeliverPosition2  % 3x1, mm
+        
         ToneDuration  % s
         ToneDelay  % s, the delay between the end of the tone and the movement to the deliver position
-        DeliverPosition  % 3x1, mm
         ReturnDelay  % s, the delay until the post returns to the home position
     end  % properties
-    
+
+    properties (SetAccess=private)
+        TrialSequence  % 1 x sweepCount, each element 1 or 2
+    end
+
     properties (Access=protected, Transient=true)
         PezDispenser_
     end
@@ -31,10 +42,22 @@ classdef PezUserClass < ws.UserClass
         end
         
         % These methods are called in the frontend process
-        function startingRun(self,wsModel) %#ok<INUSD>
+        function startingRun(self, wsModel)
             % Called just before each set of sweeps (a.k.a. each
             % "run")
             fprintf('About to start a run in PezUserClass.\n');
+            sweepCount = wsModel.NSweepsPerRun ;
+            if isequal(self.TrialSequenceMode, 'all-1') 
+                self.TrialSequence = repmat(1, [1 sweepCount]) ;  %#ok<REPMAT>
+            elseif isequal(self.TrialSequenceMode, 'all-2') 
+                self.TrialSequence = repmat(2, [1 sweepCount]) ;
+            elseif isequal(self.TrialSequenceMode, 'alternating')
+                self.TrialSequence = repmat([1 2], [1 ceil(sweepCount/2)]) ;                
+            elseif isequal(self.TrialSequenceMode, 'random') 
+                self.TrialSequence = randi(2, [1 sweepCount]) ;
+            else
+                error('Unrecognized TrialSequenceMode: %s', self.TrialSequenceMode) ;
+            end
             self.PezDispenser_ = ModularClient('COM3') ;
             self.PezDispenser_.open() ;
         end
@@ -65,15 +88,23 @@ classdef PezUserClass < ws.UserClass
             self.PezDispenser_ = [] ;
         end
         
-        function startingSweep(self,wsModel)  %#ok<INUSD>
+        function startingSweep(self,wsModel)
             % Called just before each sweep
             fprintf('About to start a sweep in PezUserClass.\n');
-            self.PezDispenser_.basePosition('setValue', self.BasePosition) ;            
-            self.PezDispenser_.toneFrequency('setValue', self.ToneFrequency) ;            
+            sweepIndex = wsModel.NSweepsCompletedInThisRun + 1 ;
+            trialType = self.TrialSequence(sweepIndex) ;
+            if trialType == 1 ,
+                self.PezDispenser_.basePosition('setValue', self.BasePosition1) ;
+                self.PezDispenser_.toneFrequency('setValue', self.ToneFrequency1) ;
+                self.PezDispenser_.deliverPosition('setValue', self.DeliverPosition1) ;
+            else
+                self.PezDispenser_.basePosition('setValue', self.BasePosition2) ;
+                self.PezDispenser_.toneFrequency('setValue', self.ToneFrequency2) ;
+                self.PezDispenser_.deliverPosition('setValue', self.DeliverPosition2) ;
+            end
             self.PezDispenser_.toneDuration('setValue', self.ToneDuration) ;            
             self.PezDispenser_.toneDelayMin('setValue', self.ToneDelay) ;            
             self.PezDispenser_.toneDelayMax('setValue', self.ToneDelay) ;            
-            self.PezDispenser_.deliverPosition('setValue', self.DeliverPosition) ;            
             self.PezDispenser_.returnDelayMin('setValue', self.ReturnDelay) ;            
             self.PezDispenser_.returnDelayMax('setValue', self.ReturnDelay) ;            
         end
@@ -93,13 +124,13 @@ classdef PezUserClass < ws.UserClass
             fprintf('Oh noes!  A sweep aborted in PezUserClass.\n');
         end        
         
-        function dataAvailable(self, wsModel)  %#ok<INUSL>
+        function dataAvailable(self, wsModel)  %#ok<INUSD>
             % Called each time a "chunk" of data (typically 100 ms worth) 
             % has been accumulated from the looper.
-            analogData = wsModel.getLatestAIData();
-            digitalData = wsModel.getLatestDIData();  %#ok<NASGU>
-            nScans = size(analogData,1);
-            fprintf('Just read %d scans of data in PezUserClass.\n', nScans);                                    
+            %analogData = wsModel.getLatestAIData();
+            %digitalData = wsModel.getLatestDIData();  %#ok<NASGU>
+            %nScans = size(analogData,1);
+            %fprintf('Just read %d scans of data in PezUserClass.\n', nScans);                                    
         end
         
         % These methods are called in the looper process
